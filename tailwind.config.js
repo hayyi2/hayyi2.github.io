304525ef083b0f8f82c35ef39972bfae3d1f348e
hayyi2 <ahmadhayyi2@gmail.com>
2023-09-01 09:04:47 +0700

    improve layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     '**/*.{html,njk,md}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {},
   },
   plugins: [
